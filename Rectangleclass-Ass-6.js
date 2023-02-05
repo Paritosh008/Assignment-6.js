21d32e85759b89a0e57669006bb2136fec4fe611
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-02-05 11:48:59 +0530

    Create Rectangleclass-Ass-6.js

---
+++ Rectangleclass-Ass-6.js
@@ -0,0 +1,20 @@
+#1.Perform the following operations to provide the implementation for aRectangle class. The operations are:
+class Rectangle{
+    constructor(length,breadth){
+        this.length = length
+        this.breadth = breadth
+    }
+    getArea(){
+        area = this.length*this.breadth
+        return area
+    }
+}
+
+class Square extends Rectangle{
+    constructor(length,breadth){
+    super(length,breadth);
+    }
+}
+
+let area = new Square(2,6)
+console.log(area.getArea())
